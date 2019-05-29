fc3dbf60d4e2e847c5d77bd2b414810170cd89a0
Serge Trinquet <strinquet@cahpp.fr>
2019-05-29 18:04:55 +0200

    Modif Connect bdd (async-await au lieu de promesses

--- app_modules/connectToBdd.js
+++ app_modules/connectToBdd.js
@@ -5,7 +5,10 @@ const colors = require('colors'); // Pour le dev.
 
 module.exports = async function(requete) {
     // Version sous forme de Promise (peut être utilisée ensuite comme une promise (=> avec '.then().then()....cath()') ou bien avec async/await )
-    return sql.connect(dbConfig)
+    sql.close(); // Ajouté car parfois bug après exec plusieurs fois de suite dans la meme sec. (et je ne sais pas pourquoi!) car pas le temps de se fermer, donc je ferme d'entrée de jeu la connection au cas ou elle serait restée ouverte ("réinitialisation" en quelque sorte)
+    
+    // V1 : Avec Promesses
+    /*return sql.connect(dbConfig)
     .then(pool => {
         return pool.request().query(requete)
     })
@@ -18,5 +21,21 @@ module.exports = async function(requete) {
         sql.close();
         err.customMsg = "Phase de connection à la base de données"
         throw err;
-    });
+    });*/
+
+
+    // V2 : Avec async/await
+     try {
+        let pool = await sql.connect(dbConfig);
+        let recordset = await pool.request().query(requete);
+        sql.close();
+        console.log(colors.bgGreen.white(JSON.stringify(recordset))); //TEST
+        return recordset;
+    } catch (err) {
+        sql.close();
+        console.log('err => ' + err); //TEST
+        err.customMsg = "Phase de connection à la base de données"
+        throw err;
+    } 
+
 }
